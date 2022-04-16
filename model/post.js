a9a19a9a92e3dd0c13b3557ba0d0fc8b7c1d89c1
Hazel Wu <tinazx056@gmail.com>
2022-04-16 15:59:01 +0800

    feat(model): create post model

---
+++ model/post.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose')
+
+const PostSchema = new Schema({
+  // 發文者名稱
+  userName: {
+    type: String,
+    required: [true, '發文者姓名為必填']
+  },
+  // 發文內容
+  userContent: {
+    type: String,
+    required: [true, '內容為必填']
+  },
+  // 使用者圖片
+  userPhoto: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+}, { versionKey: false })
+
+const Post = model('Post', PostSchema)
+
+module.exports = Post
