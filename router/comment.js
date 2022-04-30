2769580dcad6e7ef1393bff71472cc6f320b2e77
Hazel Wu <tinazx056@gmail.com>
2022-05-01 04:04:40 +0800

    revise: 修正 comment Route

--- router/comment.js
+++ router/comment.js
@@ -3,11 +3,13 @@ const router = express.Router()
 // Controller
 const commentController = require('../controller/comment.js')
 
+router.get('/:post_id', commentController.getComment) // 取得貼文留言
+
 router
   .route('/')
-  .get(commentController.getComment) // 取得貼文留言
   .post(commentController.createComment) // 新增留言
   .patch(commentController.updateComment) // 修改留言
   .delete(commentController.deleteComment) // 刪除留言
+
 
 module.exports = router
