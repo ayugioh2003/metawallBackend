8549d308c923428924ca242e7445f97ea7ffa7de
Hazel Wu <tinazx056@gmail.com>
2022-05-01 21:02:51 +0800

    feat: cors origin 改為全域開放

--- utils/cors.js
+++ utils/cors.js
@@ -1,8 +1,5 @@
 const corsOptions = {
-  origin: [
-    'https://metawall-dusky.vercel.app',
-    'http://localhost:3000',
-  ],
+  origin: '*',
   preflightContinue: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: ['Content-Type', 'Authorization'],
