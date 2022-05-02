24ca30d163f83db52c6e57459087422f7b3ca30a
Hazel Wu <tinazx056@gmail.com>
2022-05-02 18:12:33 +0800

    fix: user 缺少 updateUserInfo

--- controller/user.js
+++ controller/user.js
@@ -13,10 +13,6 @@ const ApiState = require('../utils/apiState')
   ApiState.js 可自行新增需要的錯誤內容
 */
 
-/* 取得個人資訊	GET	/users/:user_id */
-const getUserInfo = catchAsync(async (req, res, next) => {
-  return next(new AppError(ApiState.FIELD_MISSING))
-})
 
 /* 取得目前資訊	GET	/users/current-userinfo */
 const getCurrentUserInfo = catchAsync(async (req, res, next) => {
@@ -28,18 +24,28 @@ const updateCurrentUserInfo = catchAsync(async (req, res, next) => {
   successHandle({ res, message: '修改個人資訊成功' })
 })
 
+/* 取得個人資訊 GET	/users/:user_id */
+const getUserInfo = catchAsync(async (req, res, next) => {
+  return next(new AppError(ApiState.FIELD_MISSING))
+})
+
 /* 取得用戶列表	GET	/users */
 const getUserList = catchAsync(async (req, res, next) => {
   return next(new AppError(ApiState.FIELD_MISSING))
 })
 
-/* 新增個人資訊	POST /users/:user_id */
+/* 新增個人資訊 POST /users/:user_id */
 const createUserInfo = catchAsync(async (req, res, next) => {
   successHandle({ res, message: '成功' })
 })
 
-/* 刪除個人資訊	DELETE	/users/:user_id */
+/* 刪除個人資訊 DELETE /users/:user_id */
 const deleteUserInfo = catchAsync(async (req, res, next) => {
+  successHandle({ res, message: '成功' })
+})
+
+/* 修改個人資訊 PATCH /users/:user_id */
+const updateUserInfo = catchAsync(async (req, res, next) => {
   successHandle({ res, message: '成功' })
 })
 
@@ -51,5 +57,5 @@ module.exports = {
   getUserList,
   createUserInfo,
   deleteUserInfo,
-  updateUserInfo,
+  updateUserInfo
 }
