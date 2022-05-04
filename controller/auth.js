b4f3556780c08eab80294653ed291d0aff4db597
ming wu <a85062@pchome.com.tw>
2022-05-04 21:35:05 +0800

    【新增】會員註冊 【新增】會員登入

--- controller/auth.js
+++ controller/auth.js
@@ -1,9 +1,14 @@
 // Model
-const User = require('../model/user')
+const User = require('../model/user');
 // Utils
-const catchAsync = require('../utils/catchAsync')
-const AppError = require('../utils/appError')
-const { successHandle } = require('../utils/resHandle.js')
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const apiState = require('../utils/apiState');
+const { successHandle } = require('../utils/resHandle.js');
+const { verifyToken, checkEmail } = require('../utils/verification');
+const { hashPassword } = require('../utils/hash');
+// jwt
+const jwt = require('jsonwebtoken');
 
 /*
   res 回傳錯誤範例
@@ -12,38 +17,144 @@ const { successHandle } = require('../utils/resHandle.js')
   ApiState.js 可自行新增需要的錯誤內容
 */
 
-
 /*
   登入功能	POST	/login
 */
 const login = catchAsync(async (req, res, next) => {
-  successHandle({ res, message: '登入成功' })
-})
+  try {
+    const memberData = {
+      email: req.body.email,
+      password: hashPassword(req.body.password),
+    };
+    User.findOne({
+      email: memberData.email,
+      password: memberData.password,
+    }).exec((err, findRes) => {
+      if (err) {
+        return successHandle({
+          res,
+          status: false,
+          statusCode: 404,
+          message: '登入失敗',
+          data: new AppError(apiState.INTERNAL_SERVER_ERROR).message,
+        });
+      }
+      if (findRes === null) {
+        return successHandle({
+          res,
+          status: false,
+          statusCode: 404,
+          message: '登入失敗',
+          data: new AppError(apiState.DATA_NOT_EXIST).message,
+        });
+      } else {
+        const token = jwt.sign(
+          {
+            // 加密方式
+            algorithm: 'HS256',
+            // 多久之後到期 60一分鐘到期 60*60一小時 也可以不用exp直接在secret後面加上{ expiresIn: '1h' }
+            exp: Math.floor(Date.now() / 1000) + 60 * 60,
+            // data的內容可以在登入解密出來
+            data: findRes._id,
+          },
+          // 給jwt一個字串當作加密編碼參考 需要隱藏起來 否則會有被反推的機會
+          // 驗證的時候要用一樣的字串去解 不然會算不出原本的資料
+          'secret'
+        );
+        res.setHeader('token', token);
+        findRes.token = token;
+        return successHandle({
+          res,
+          message: '登入成功',
+          data: {
+            user:findRes,
+            token:findRes.token
+          },
+        });
+      }
+    });
+  } catch (error) {
+    return successHandle({
+      res,
+      status: false,
+      statusCode: 404,
+      message: '登入失敗',
+      data: new AppError(apiState.SYNTAX_ERROR).message,
+    });
+  }
+});
 
 /*
   註冊功能	POST	/signup
 */
 const signup = catchAsync(async (req, res, next) => {
-  successHandle({ res, message: '註冊成功' })
-})
+  try {
+    const memberData = {
+      name: req.body.name,
+      email: req.body.email,
+      password: hashPassword(req.body.password),
+    };
+    if (checkEmail(req.body.email)) {
+      User.findOne({ email: memberData.email }).exec((err, res) => {
+        if (err) {
+          return successHandle({
+            res,
+            status: false,
+            statusCode: 404,
+            message: '註冊失敗',
+            data: new AppError(apiState.INTERNAL_SERVER_ERROR).message,
+          });
+        }
+      });
+      try {
+        const data = await User.create(memberData);
+        return successHandle({ res, message: '註冊成功', data });
+      } catch (error) {
+        return successHandle({
+          res,
+          status: false,
+          statusCode: 404,
+          message: '註冊失敗',
+          data: new AppError(apiState.DATA_EXIST).message,
+        });
+      }
+    } else {
+      return successHandle({
+        res,
+        status: false,
+        statusCode: 404,
+        message: '註冊失敗',
+        data: new AppError(apiState.FIELD_MISSING).message,
+      });
+    }
+  } catch (error) {
+    return successHandle({
+      res,
+      status: false,
+      statusCode: 404,
+      message: '註冊失敗',
+      data: new AppError(apiState.SYNTAX_ERROR).message,
+    });
+  }
+});
 
 /*
   登出功能	GET	/logout
 */
 const logout = catchAsync(async (req, res, next) => {
-  successHandle({ res, message: '登出成功' })
-})
+  successHandle({ res, message: '登出成功' });
+});
 
 /*
   修改密碼	PATCH	/reset-password
 */
 const resetPassword = catchAsync(async (req, res, next) => {
-  successHandle({ res, message: '修改密碼成功' })
-})
+  successHandle({ res, message: '修改密碼成功' });
+});
 
 module.exports = {
   login,
   signup,
   logout,
-  resetPassword
-}
+  resetPassword,
+};
--- router/auth.js
+++ router/auth.js
@@ -6,9 +6,9 @@ const authController = require('../controller/auth.js')
 const userController = require('../controller/user.js')
 
 // 登入
-router.post('/signup', authController.login)
+router.post('/login', authController.login)
 // 註冊
-router.post('/login', authController.signup)
+router.post('/signup', authController.signup)
 // 登出
 router.post('/logout', authController.logout)
 // 修改密碼
