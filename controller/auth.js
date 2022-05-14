0edb43e5acc091ef9d6d67209d33223d56abc5c3
ming wu <a85062@pchome.com.tw>
2022-05-14 22:32:35 +0800

    【新增】驗證密碼正規表達式

--- controller/auth.js
+++ controller/auth.js
@@ -1,10 +1,10 @@
 // Model
-const User = require('../model/user')
+const User = require('../model/user');
 // Utils
-const catchAsync = require('../utils/catchAsync')
-const AppError = require('../utils/appError')
-const { successHandle } = require('../utils/resHandle.js')
-const { verifyToken, checkEmail } = require('../utils/verification');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const { successHandle } = require('../utils/resHandle.js');
+const {  checkEmail,checkPassword } = require('../utils/verification');
 const { hashPassword } = require('../utils/hash');
 
 /*
@@ -14,60 +14,83 @@ const { hashPassword } = require('../utils/hash');
   ApiState.js 可自行新增需要的錯誤內容
 */
 
-
 /*
   登入功能	POST	/login
 */
 const login = catchAsync(async (req, res, next) => {
-  successHandle({ res, message: '登入成功' })
-})
+  successHandle({ res, message: '登入成功' });
+});
 
 /*
   註冊功能	POST	/signup
 */
 const signup = catchAsync(async (req, res, next) => {
-console.log(req.body);
-  const memberData = {
-    name: req.body.name,
-    email: req.body.email,
-    password: hashPassword(req.body.password),
-  };
-
+  console.log(req.body);
+  if(!checkPassword(req.body.password)){
+    return next(
+      new AppError(
+        ApiState.FIELD_MISSING.message,
+        ApiState.FIELD_MISSING.statusCode
+      )
+    );
+  }
   if (checkEmail(req.body.email)) {
-    User.findOne({ email: memberData.email }).exec((err, res) => {
-      if (err) {
-        return next(new AppError(ApiState.INTERNAL_SERVER_ERROR.message, ApiState.INTERNAL_SERVER_ERROR.statusCode));
+    const memberData = {
+      name: req.body.name,
+      email: req.body.email,
+      password: hashPassword(req.body.password),
+    };
+    User.findOne({ email: memberData.email }, '_id name email').exec(
+      (err, res) => {
+        if (err) {
+          return next(
+            new AppError(
+              ApiState.INTERNAL_SERVER_ERROR.message,
+              ApiState.INTERNAL_SERVER_ERROR.statusCode
+            )
+          );
+        }
       }
-    });
+    );
     try {
-      const data = await User.create(memberData);
+      const result = await User.create(memberData);
       return successHandle({ res, message: '註冊成功', data });
     } catch (error) {
       // 帳號已存在
-      return next(new AppError(ApiState.DATA_EXIST.message, ApiState.DATA_EXIST.statusCode));
+      return next(
+        new AppError(
+          ApiState.DATA_EXIST.message,
+          ApiState.DATA_EXIST.statusCode
+        )
+      );
     }
   } else {
-    return next(new AppError(ApiState.FIELD_MISSING.message, ApiState.FIELD_MISSING.statusCode));
+    return next(
+      new AppError(
+        ApiState.FIELD_MISSING.message,
+        ApiState.FIELD_MISSING.statusCode
+      )
+    );
   }
-})
+});
 
 /*
   登出功能	GET	/logout
 */
 const logout = catchAsync(async (req, res, next) => {
-  successHandle({ res, message: '登出成功' })
-})
+  successHandle({ res, message: '登出成功' });
+});
 
 /*
   修改密碼	PATCH	/reset-password
 */
 const resetPassword = catchAsync(async (req, res, next) => {
-  successHandle({ res, message: '修改密碼成功' })
-})
+  successHandle({ res, message: '修改密碼成功' });
+});
 
 module.exports = {
   login,
   signup,
   logout,
-  resetPassword
-}
+  resetPassword,
+};
