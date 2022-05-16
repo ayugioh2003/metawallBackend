d6a810ca9ce922af82f14119f521f00173a692ba
Hazel Wu <tinazx056@gmail.com>
2022-05-16 21:22:22 +0800

    fix(Model): 修復 Model 設定 createdAt, updatedAt 不會自動更新時間

--- model/image.js
+++ model/image.js
@@ -10,18 +10,22 @@ const ImageSchema = new mongoose.Schema({
     type: String,
     required: [true, '圖片網址為必填']
   },
+
   // 建立時間，轉為 Timestamp 以方便前端好處理
   createdAt: {
     type: Number,
-    default: new Date().getTime(),
   },
 
   // 更新時間，轉為 Timestamp 以方便前端好處理
   updatedAt: {
     type: Number,
-    default: new Date().getTime(),
   },
-}, { versionKey: false })
+}, {
+  versionKey: false,
+  timestamps: {
+    currentTime: () => Date.now()
+  }
+})
 
 const Image = mongoose.model('Image', ImageSchema)
 module.exports = Image
