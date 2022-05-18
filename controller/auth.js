01ad8a42e8012f37c1a1df2df134e3950712661f
ayugioh2003 <ayugioh2003@gmail.com>
2022-05-19 00:05:59 +0800

    fix: secret & expiresIn only use env to get value

--- controller/auth.js
+++ controller/auth.js
@@ -71,13 +71,13 @@ const login = catchAsync(async (req, res, next) => {
       },
       // 給jwt一個字串當作加密編碼參考 需要隱藏起來 否則會有被反推的機會
       // 驗證的時候要用一樣的字串去解 不然會算不出原本的資料
-      process.env.SECRET || 'secret',
+      process.env.SECRET,
       { 
         algorithm: 'HS256', // 加密方式
         // 多久之後到期 60一分鐘到期 60*60一小時
         // 也可以不用exp直接在secret後面加上{ expiresIn: '1h' }
         // exp: Math.floor(Date.now() / 1000) + 60 * 60,
-        expiresIn: process.env.EXPIRES_IN || '1h'
+        expiresIn: process.env.EXPIRES_IN
       }
     );
 
