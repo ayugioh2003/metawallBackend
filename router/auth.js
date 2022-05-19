7b6c147ca4f9bb2c54668217c603ca5bbdc16dfc
ming wu <a85062@pchome.com.tw>
2022-05-19 19:38:22 +0800

    【新增】驗證用middleware

--- router/auth.js
+++ router/auth.js
@@ -1,9 +1,30 @@
 const express = require('express')
 
 const router = express.Router()
+// utils
+const {verifyToken} = require('../utils/verification')
 // Controller
 const authController = require('../controller/auth.js')
 const userController = require('../controller/user.js')
+// 驗證用middleware
+const token = (req,res,next)=>{
+  const id = req.body.id;
+  const token = req.headers.token;
+   // 取的token驗證通過解密出來的使用者id
+   const verify = await verifyToken(token);
+   if (verify && verify === id){
+     console.log('驗證通過');
+     next();
+   }
+   else{
+     return next(
+       new AppError({
+         message: '帳號與token不符合',
+         statusCode: ApiState.DATA_NOT_EXIST.statusCode,
+       })
+     );
+   }
+}
 
 // 登入
 router.post('/signup', authController.signup)
