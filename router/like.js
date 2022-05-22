83db6cc5499bc338fffd812804d8ee73251ae37b
Hazel Wu <tinazx056@gmail.com>
2022-05-23 02:04:39 +0800

    feat(router): 調整 likes 路由，加上 isAuth

--- router/like.js
+++ router/like.js
@@ -1,12 +1,13 @@
 const express = require('express')
 
 const router = express.Router()
+// Controller
 const likeController = require('../controller/like.js')
+const { isAuth } = require('../controller/auth.js')
 
 router
   .route('/')
-  .get(likeController.getLike) // 取得貼文按讚、取得留言按讚
-  .patch(likeController.toggleLike) // 切換貼文按讚狀態、切換留言按讚狀態
-
+  .get(isAuth, likeController.getLike) // 取得貼文按讚、取得留言按讚
+  .patch(isAuth, likeController.toggleLike) // 切換貼文按讚狀態、切換留言按讚狀態
 
 module.exports = router
