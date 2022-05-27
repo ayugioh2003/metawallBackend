23d5647f1a2a21928ce7a77458ba24d2ce7dff8c
Amber <amber871023@gmail.com>
2022-05-27 21:50:05 +0800

    Feature/following

--- model/following.js
+++ model/following.js
@@ -5,9 +5,32 @@ const validator = require('validator')
  * Follow Model
  * 追蹤模組
 */
-const FollowingSchema = new mongoose.Schema({
+const FollowingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+    },
+    following: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+    },
+    // 建立時間，轉為 Timestamp 以方便前端好處理
+    createdAt: {
+      type: Number,
+    },
+    // 更新時間，轉為 Timestamp 以方便前端好處理
+    updatedAt: {
+      type: Number,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: {
+      currentTime: () => Date.now(),
+    },
+  },
+)
 
-})
-
-const Following = mongoose.model(FollowingSchema)
+const Following = mongoose.model('Following', FollowingSchema)
 module.exports = Following
