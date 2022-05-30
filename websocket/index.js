3b83a0ce57a4a8b9c0c2baf70e84eea8c231d82b
ming wu <a85062@pchome.com.tw>
2022-05-30 19:43:22 +0800

    【更新】server.js使用create server 【新增】websocket資料夾

---
+++ websocket/index.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-console */
+/* eslint-disable no-unsafe-optional-chaining */
+const WebSocket = require('ws')
+
+const wsServer = (expressServer) => {
+  // const wsUser = []
+
+  // 建立新的ws伺服器
+  const websocketServer = new WebSocket.Server({
+    server: expressServer,
+  })
+
+  websocketServer.on(
+    'connection',
+    async (websocketConnection, connectionRequest) => {
+      const [_path, params] = connectionRequest?.url?.split('?')
+      console.log(_path)
+      console.log(params)
+      console.log(`目前連線人數 : ${websocketServer._server._connections}`)
+      websocketConnection.userid = params
+
+      // wsUser.push({ userid: params, ws: websocketConnection })
+
+      websocketConnection.on('message', async (message) => {
+        const msgData = JSON.parse(message)
+
+        websocketServer.clients.forEach((client) => {
+          client.send(JSON.stringify(msgData))
+        })
+      })
+    },
+  )
+
+  return websocketServer
+}
+
+module.exports = wsServer
