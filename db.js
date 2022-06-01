932dce7b9991e06e8e9d9a3cc9a649051f3872a5
Hazel Wu <tinazx056@gmail.com>
2022-06-01 13:17:17 +0800

    fix(db): 助教回饋 db.js 建議

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-console */
+/* eslint-disable */
 const mongoose = require('mongoose')
 
 // 遠端資料庫
@@ -8,12 +7,12 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 )
 
-// 連接本地資料庫
+// 連接本地資料庫，這裡留著是方便本地開發切換使用，故不刪除
 const localDB = process.env.DATABASE_LOCAL
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(DB)
+    await mongoose.connect(DB || localDB)
     console.log('MongoDB 資料庫連接成功')
   } catch (error) {
     console.log('連接資料庫失敗：', error)
