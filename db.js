3b4501bcf2ce341a50347e4d6f26ab1e8d58e047
Hazel Wu <tinazx056@gmail.com>
2022-06-02 22:56:09 +0800

    fix: db.js 註解 localDB

--- db.js
+++ db.js
@@ -7,12 +7,12 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 )
 
-// 連接本地資料庫，這裡留著是方便本地開發切換使用，故不刪除
-const localDB = process.env.DATABASE_LOCAL
+// 連接本地資料庫，這裡留著是方便本地開發切換使用，故不刪除先註解
+// const localDB = process.env.DATABASE_LOCAL
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(DB || localDB)
+    await mongoose.connect(DB)
     console.log('MongoDB 資料庫連接成功')
   } catch (error) {
     console.log('連接資料庫失敗：', error)
