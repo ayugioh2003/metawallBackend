20744e349c21711579c677f763cc7ef49ff5e660
ming wu <a85062@pchome.com.tw>
2022-06-05 23:02:24 +0800

    【修改】回傳資料格式

--- controller/message.js
+++ controller/message.js
@@ -9,11 +9,9 @@ const Message = require('../model/message.js')
 
 // 取得訊息列表
 const getMessages = catchAsync(async (req, res, next) => {
-  const message = await Message.find()
-    .populate({
-      path: 'user',
-      select: '_id name avatar',
-    })
+  const message = await Message.find().populate({
+    path: 'user',
+  })
   if (!message) return next(new AppError(ApiState.DATA_NOT_EXIST))
   console.log(message)
   successHandle({
