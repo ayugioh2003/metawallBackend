a4114009762d36e30a8caa25e930656e7abdbbae
John Chiu <john@makertec.net>
2022-06-10 13:51:56 +0800

    fix(newebpay): returnURL 多增加被贊助者名字

--- controller/newebpay.js
+++ controller/newebpay.js
@@ -97,15 +97,23 @@ const returnURL = catchAsync(async (req, res, next) => {
   }
 
   const userId = orderRes.donateTo
-  const { Comment, donateFrom, Amt } = orderRes
+  const {
+    Comment, donateFrom, donateTo, Amt,
+  } = orderRes
 
-  const userRes = await User.findById(donateFrom)
-  if (!userRes) {
+  const donateFromRes = await User.findById(donateFrom)
+  if (!donateFromRes) {
     return next(new AppError({ message: '找不到此贊助者', statusCode: 400 }))
   }
-  const donateName = userRes.name
+  const donateFromName = donateFromRes.name
 
-  const URL = `${process.env.FRONTEND_URL}/userWall/${userId}?from=returnURL&comment=${Comment}&donateFrom=${donateName}&amt=${Amt}`
+  const donateToRes = await User.findById(donateTo)
+  if (!donateToRes) {
+    return next(new AppError({ message: '找不到此被贊助者', statusCode: 400 }))
+  }
+  const donateToName = donateToRes.name
+
+  const URL = `${process.env.FRONTEND_URL}/userWall/${userId}?from=returnURL&comment=${Comment}&donateFrom=${donateFromName}&donateTo=${donateToName}&amt=${Amt}`
   res.redirect(URL)
 })
 
